bff7e87c870ed30e6190889bc2bfecdd195836fc
Oscar Alejandro <oscarnavarroaviles@gmail.com>
2023-01-26 22:34:04 +0100

    Routers structure and home page, about page was created

---
+++ src/pages/About.js
@@ -0,0 +1,11 @@
+export const About = () => {
+    return (
+      <div className="about">
+          <h2>About Us</h2>
+          <p>Lorem ipsum dolor sit amet consectur adipisicing elit. Rerum dolor assumeda, itaque nesciunt alias voluptatum nobis blanditiis eos officiis aperiam earum eum</p>
+          <p>Lorem ipsum dolor sit amet consectur adipisicing elit. Rerum dolor assumeda, itaque nesciunt alias voluptatum nobis blanditiis eos officiis aperiam earum eum</p>
+          <p>Lorem ipsum dolor sit amet consectur adipisicing elit. Rerum dolor assumeda, itaque nesciunt alias voluptatum nobis blanditiis eos officiis aperiam earum eum</p>
+      </div>
+    )
+  }
+  
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+
+export const Home = () => {
+  return (
+    <div className="home">
+        <h2>Welcome</h2>
+        <p>Lorem ipsum dolor sit amet consectur adipisicing elit. Rerum dolor assumeda, itaque nesciunt alias voluptatum nobis blanditiis eos officiis aperiam earum eum</p>
+        <p>Lorem ipsum dolor sit amet consectur adipisicing elit. Rerum dolor assumeda, itaque nesciunt alias voluptatum nobis blanditiis eos officiis aperiam earum eum</p>
+        <p>Lorem ipsum dolor sit amet consectur adipisicing elit. Rerum dolor assumeda, itaque nesciunt alias voluptatum nobis blanditiis eos officiis aperiam earum eum</p>
+    </div>
+  )
+}
