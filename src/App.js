f97b2e85ea9493209b1e53fa5e798a9255e2548f
Oscar Alejandro <oscarnavarroaviles@gmail.com>
2023-01-27 21:27:37 +0100

    I use new way to make route with createBrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Route, 
+  NavLink, 
+  createRoutesFromElements,
+  RouterProvider
+} from "react-router-dom";
+
 //pages
 import { About, Home } from "./pages";
 
+// layouts
+import { RootLayout } from "./layouts/RootLayout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout/>}>
+      <Route index element={<Home/>} />
+      <Route path="about" element={<About/>} />
+    </Route>
+  )
+)
+
 function App() {
   return (
-    <BrowserRouter>
-      <header>
-        <nav>
-          <h1>Jobarouter</h1>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="about">About</NavLink>
-        </nav>
-      </header>
-      <main>
-        <Routes>
-          <Route index element={<Home/>} />
-          <Route path="about" element={<About/>} />
-        </Routes>
-      </main>
-    </BrowserRouter>
+      <RouterProvider router={router} />
   );
 }
 
