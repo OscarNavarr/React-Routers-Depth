6d222b41fefebc182d898dac888e9a6ef9c8b539
Oscar Alejandro <oscarnavarroaviles@gmail.com>
2023-02-03 19:18:28 +0100

    Project finished

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,20 @@
+import { useState } from "react"
+import { Navigate } from "react-router-dom";
+
 export const About = () => {
+    const [user, setUser] = useState('mario');
+
+    if(!user){
+      return <Navigate to="/" replace={true}/>
+    }
     return (
       <div className="about">
           <h2>About Us</h2>
           <p>Lorem ipsum dolor sit amet consectur adipisicing elit. Rerum dolor assumeda, itaque nesciunt alias voluptatum nobis blanditiis eos officiis aperiam earum eum</p>
           <p>Lorem ipsum dolor sit amet consectur adipisicing elit. Rerum dolor assumeda, itaque nesciunt alias voluptatum nobis blanditiis eos officiis aperiam earum eum</p>
           <p>Lorem ipsum dolor sit amet consectur adipisicing elit. Rerum dolor assumeda, itaque nesciunt alias voluptatum nobis blanditiis eos officiis aperiam earum eum</p>
+
+          <button onClick={() => setUser(null)}>Logout</button>
       </div>
     )
   }
